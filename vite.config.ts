cfbe609ce73e4f9c68bc585e119b5854d90ea933
steven <scoaila@proton.me>
2025-06-02 17:14:42 -0500

    fix: update base path in vite.config.ts to use the correct repository name

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// Sustituye "mi-repo" por el nombre de tu repositorio en GitHub
-const repoName = 'tinkuy';
-
 export default defineConfig({
-  base: `/${repoName}`, // ← Agrega esta línea
+  base: `/tinkuy/`, // ← Agrega esta línea
   server: {
     host: '::',
     port: 8080,
