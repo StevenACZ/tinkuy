e6791d316915df3c2fdace297b45a603abd76ea6
steven <scoaila@proton.me>
2025-06-02 16:52:22 -0500

    feat: update package.json and vite.config.ts for deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
+// Sustituye "mi-repo" por el nombre de tu repositorio en GitHub
+const repoName = 'tinkuy';
+
 export default defineConfig({
+  base: `/${repoName}/`, // ← Agrega esta línea
   server: {
     host: '::',
     port: 8080,
